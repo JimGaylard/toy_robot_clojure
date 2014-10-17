09b2c80cf6aa036730543f94af13702416cd12ea
Jim Gaylard <jim.gaylard@rea-group.com>
2014-10-17 19:38:13 +1100

    Basic robot methods

--- src/toy_robot/core.clj
+++ src/toy_robot/core.clj
@@ -1,7 +1,49 @@
 (ns toy-robot.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def directions
+  {:NORTH [0 1]
+   :EAST [1 0]
+   :SOUTH [0 -1]
+   :WEST [-1 0]})
+
+(def compass
+  [:NORTH :EAST :SOUTH :WEST])
+
+(defn create-robot
+  "Returns a robot object with coordinates and facing"
+  [coordinates facing]
+  (let [[x y] coordinates]
+  {:coordinates {:x x :y y} :facing facing}))
+
+(defn place
+  "Function that takes an x and y coordinate plus a facing to place the robot"
+  [x y facing]
+  (create-robot [x y] facing))
+
+(defn move
+  "Moves the robot forward one block in the current facing direction"
+  [robot]
+  (let [{{x :x y :y} :coordinates
+        facing :facing} robot]
+    (create-robot
+      (map + [x y] (directions facing))
+      facing)))
+
+(defn turn
+  "Turn the robot left or right and return a robot with the new direction"
+  [robot direction]
+  (let [{facing :facing} robot
+        turning (direction {:left -1 :right 1})]
+    (assoc robot :facing
+                (first (rotate
+                  turning
+                  (rotate (.indexOf compass facing)
+                         compass))))))
+
+(defn rotate
+  "Rotates a sequence left (negative) or right (positive) by the magnitude
+  provided."
+  [n s]
+  (let [c (count s)]
+    (take c (drop (mod n c) (cycle s)))))
